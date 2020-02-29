3aab38b9a1aa1097d9c1ae58218e8d2fa4744c52
UzaifBajwa <47759898+UzaifBajwa@users.noreply.github.com>
2020-02-29 17:34:33 +0500

    Update test_1.php
    Hi Faisal, 
    have a look at the output. and let me know how it went.
    
    Best Regard,
    Uzaif

--- test_1.php
+++ test_1.php
@@ -11,3 +11,23 @@
 
 
 #e2f64796272757260237960227567737e6160256864502e256d69647023796864702e696770257f69502e24757364727f686370256864702b6f6f6470257f695
+
+echo getFileDecrypted();
+
+
+function getFileDecrypted ()
+	{
+		$myfile = fopen("test_1.txt", "a+") or die("Unable to open the file!");
+		$text = file_get_contents('test_1.txt');
+		$text = nl2br($text);
+
+		$newText = '';
+		// conversion from hex to dec goes here
+		for ($i=0; $i < strlen($text)-1; $i+=2){
+        $newText .= chr(hexdec($text[$i].$text[$i+1]));
+    }
+    return $newText;
+		echo $newText;
+    fclose($myfile);
+	}
+?>
